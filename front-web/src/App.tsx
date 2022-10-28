8ec601492daab28218b4dd447b4985849f8a3d83
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-10-28 11:12:43 -0300

    Header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
