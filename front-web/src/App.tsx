2e0ed963e8a4cc03340d8721bfc61ef49dd61c7d
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-10-28 15:02:47 -0300

    Integração do backend com o componente filter

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import Filter, { StoreFilterData } from './components/filter';
 import Header from './components/header';
 
 function App() {
+  const [filterData, setFilterData] = useState<StoreFilterData>();
+
+  const onFilterChange = (filter: StoreFilterData) => {
+    setFilterData(filter);
+  };
   return (
     <>
       <Header />
+      <div className="app-container">
+        <Filter onFilterChange={onFilterChange} />
+      </div>
     </>
   );
 }
---
+++ front-web/src/components/filter/index.tsx
@@ -0,0 +1,64 @@
+import './styles.css';
+import Select from 'react-select';
+import { Controller, useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { Store } from '../../types/store';
+import { makeRequest } from '../../util/requests';
+
+export type StoreFilterData = {
+  name: Store | null;
+};
+
+type Props = {
+  onFilterChange: (filter: StoreFilterData) => void;
+};
+
+function Filter({ onFilterChange }: Props) {
+  const [stores, setStores] = useState<Store[]>([]);
+
+  const { handleSubmit, setValue, control, getValues } = useForm<StoreFilterData>();
+
+  const onSubmit = (formData: StoreFilterData) => {
+    onFilterChange(formData);
+  };
+
+  const handleChangeStore = (value: Store) => {
+    setValue('name', value);
+
+    const obj: StoreFilterData = {
+      name: getValues('name')
+    };
+
+    onFilterChange(obj);
+  };
+
+  useEffect(() => {
+    makeRequest.get('/stores').then((response) => {
+      setStores(response.data);
+    });
+  }, []);
+
+  return (
+    <section className="filter-container base-card">
+      <form onSubmit={handleSubmit(onSubmit)} className="filter-form">
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => (
+            <Select
+              {...field}
+              options={stores}
+              isClearable
+              classNamePrefix="filter-select"
+              onChange={(value) => handleChangeStore(value as Store)}
+              getOptionLabel={(store: Store) => store.name}
+              getOptionValue={(store: Store) => String(store.id)}
+            />
+          )}
+        />
+      </form>
+    </section>
+  );
+}
+
+export default Filter;
